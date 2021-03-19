13b31e72dd4d48e51dbd219c30ca000b5da55132
Anton Ždanov <azdanov@pm.me>
2021-03-19 22:45:26 +0200

    add SongPlaylist

--- src/components/SongPlaylist.js
+++ src/components/SongPlaylist.js
@@ -1,7 +1,76 @@
+import {Avatar, IconButton, makeStyles, Typography} from '@material-ui/core';
+import {Delete} from '@material-ui/icons';
+import PropTypes from 'prop-types';
 import React from 'react';
 
+const useStyles = makeStyles({
+	avatar: {
+		width: 44,
+		height: 44
+	},
+	text: {
+		textOverflow: 'ellipsis',
+		overflow: 'hidden'
+	},
+	container: {
+		display: 'grid',
+		gridAutoFlow: 'column',
+		gridTemplateColumns: '50px auto 50px',
+		gridGap: 12,
+		alignItems: 'center',
+		marginTop: 10
+	},
+	songInfoContainer: {
+		overflow: 'hidden',
+		whiteSpace: 'nowrap'
+	}
+});
+
+const PlaylistItem = ({artist, title, thumbnail}) => {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.container}>
+			<Avatar src={thumbnail} alt="Song thumbnail" className={classes.avatar}/>
+			<div className={classes.songInfoContainer}>
+				<Typography variant="subtitle2" className={classes.text}>
+					{title}
+				</Typography>
+				<Typography variant="body2" className={classes.text}>
+					{artist}
+				</Typography>
+			</div>
+			<IconButton>
+				<Delete color="error"/>
+			</IconButton>
+		</div>
+	);
+};
+
+PlaylistItem.propTypes = {
+	artist: PropTypes.string.isRequired,
+	thumbnail: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired
+};
+
 const SongPlaylist = () => {
-	return <div>SongPlaylist</div>;
+	const song = {
+		artist: 'The Midnight',
+		title: '\'Ghost in Your Stereo\' (Official Lyric Video)',
+		thumbnail:
+			'https://i.ytimg.com/an_webp/xo3yRQgggwY/mqdefault_6s.webp?du=3000&sqp=CPi204IG&rs=AOn4CLDtY5G7IVvWCTkR23B0j3ps7UvXlg'
+	};
+
+	return (
+		<div style={{margin: '10px 0'}}>
+			<Typography color="textSecondary" variant="button">
+				Queue (5)
+			</Typography>
+			{Array.from({length: 5}, () => song).map((song, index) => (
+				<PlaylistItem key={index} {...song}/>
+			))}
+		</div>
+	);
 };
 
 export default SongPlaylist;
