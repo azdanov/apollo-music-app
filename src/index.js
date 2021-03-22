e34018dfd112da7db68ca19112e88cae834c40ff
Anton Ždanov <azdanov@pm.me>
2021-03-22 22:58:59 +0200

    add apollo client

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import {ApolloProvider} from '@apollo/client';
 import {CssBaseline, MuiThemeProvider} from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import client from './graphql/client.js';
 import theme from './theme.js';
 
 ReactDOM.render(
-	<React.StrictMode>
+	<ApolloProvider client={client}>
 		<MuiThemeProvider theme={theme}>
 			<CssBaseline/>
 			<App/>
 		</MuiThemeProvider>
-	</React.StrictMode>,
+	</ApolloProvider>,
 	document.querySelector('#root')
 );
