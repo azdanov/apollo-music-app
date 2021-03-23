7b65975b9ccbfca4f155939d800d64c1f809cf18
Anton Ždanov <azdanov@pm.me>
2021-03-23 22:38:44 +0200

    setup split client for websockets

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,29 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, HttpLink, InMemoryCache, split} from '@apollo/client';
+import {WebSocketLink} from '@apollo/client/link/ws';
+import {getMainDefinition} from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+	uri: 'https://graphql-music-app.herokuapp.com/v1/graphql'
+});
+
+const wsLink = new WebSocketLink({
+	uri: 'wss://graphql-music-app.herokuapp.com/v1/graphql',
+	options: {
+		reconnect: true
+	}
+});
+
+const splitLink = split(
+	({query}) => {
+		const definition = getMainDefinition(query);
+		return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+	},
+	wsLink,
+	httpLink
+);
 
 const client = new ApolloClient({
-	uri: 'https://graphql-music-app.herokuapp.com/v1/graphql',
+	link: splitLink,
 	cache: new InMemoryCache()
 });
 
